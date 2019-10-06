3cc83e247e5778b62fe3b0d1ff0f757f2444095c
Nathan Pickard <nathan.pickard87@gmail.com>
2019-10-06 16:37:47 -0700

    Creating todos and adding to backend apiService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { APIService } from './API.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-amplify-graphql';
+export class AppComponent implements OnInit {
+
+  todos: Array<any>;
+
+  constructor(private apiService: APIService) { }
+
+  async ngOnInit() {
+    this.apiService.OnCreateTodoListener.subscribe((evt) => {
+      const data = (evt as any).value.data.OnCreateTodo;
+      this.todos = [...this.todos, data];
+    });
+
+    this.apiService.ListTodos().then((evt) => {
+      this.todos = evt.items;
+    });
+
+  }
+
+  createTodo() {
+    this.apiService.CreateTodo({
+      name: 'Angular',
+      description: 'testing'
+    });
+  }
 }
