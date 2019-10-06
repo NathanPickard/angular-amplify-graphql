c984f2c8be3fcf59d50427d2c5b0dfa0a0d67fa7
Nathan Pickard <nathan.pickard87@gmail.com>
2019-10-06 14:19:39 -0700

    Integrating aws amplify and graphql backend

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import PubSub from '@aws-amplify/pubsub';
+import API from '@aws-amplify/api';
+import awsconfig from './aws-exports';
+
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
+
+API.configure(awsconfig);
+PubSub.configure(awsconfig);
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
